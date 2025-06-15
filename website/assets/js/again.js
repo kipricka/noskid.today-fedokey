02934f441e4146e8e9565725f3671798b40c061b
douxxtech <douxx@douxx.tech>
2025-06-15 23:51:23 +0200

    edited again to show the light version

--- website/assets/js/again.js
+++ website/assets/js/again.js
@@ -1,17 +1,17 @@
 //Again.js | Noskid.. in noskid ? maybe in noskid too.
 
 function againAndAgain() {
-    const iframe = `<iframe style="width: 100%; height: 100%; border: none;" src=${window.location.href}></iframe>`
+    const iframe = `<iframe style="width: 100%; height: 100%; border: none;" src="https://light.noskid.today"></iframe>`
 
     const win = ClassicWindow.createWindow({
         title: `${window.location.hostname}`,
         content: iframe,
-        width: 500,
-        height: 400,
-        x: Math.round((window.innerWidth - 500) / 2),
-        y: Math.round((window.innerHeight - 400) / 2),
+        width: 900,
+        height: 700,
+        x: Math.round((window.innerWidth - 900) / 2),
+        y: Math.round((window.innerHeight - 700) / 2),
         theme: 'dark',
-        icon: "https://images.dpip.lol/logo.png",
+        icon: "https://light.noskid.today/favicon.svg",
         statusText: "Why?.."
     });
 
