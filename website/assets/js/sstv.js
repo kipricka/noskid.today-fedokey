1c9aa67a8df6ff7957bb99e967f86260cb1a4cd4
douxxtech <douxx@douxx.tech>
2025-08-08 21:30:27 +0200

    added try-catch

--- website/assets/js/sstv.js
+++ website/assets/js/sstv.js
@@ -1,25 +1,30 @@
 // Sstv.js | Slow Scan Television :3
 
 function showSstv() {
-    const win = ClassicWindow.createWindow({
-        title: 'Oh ?',
-        content: 'Try to find the secret certificate ;)',
-        x: Math.round((window.innerWidth - 600) / 2),
-        y: Math.round((window.innerHeight - 450) / 2),
-        theme: 'dark',
-        resizable: true,
-        onClose: function () {
-            audio.pause();
-            audio.currentTime = 0;
-            audio.remove();
-        }
-    });
 
-    const sstvAudio = document.createElement('audio');
-    sstvAudio.src = 'assets/audio/nskdsstv.mp3';
-    sstvAudio.loop = true;
-    sstvAudio.style.display = 'none';
-    win.appendChild(sstvAudio);
+    try {
+        const win = ClassicWindow.createWindow({
+            title: 'Oh ?',
+            content: 'Try to find the secret certificate ;)',
+            x: Math.round((window.innerWidth - 600) / 2),
+            y: Math.round((window.innerHeight - 450) / 2),
+            theme: 'dark',
+            resizable: true,
+            onClose: function () {
+                audio.pause();
+                audio.currentTime = 0;
+                audio.remove();
+            }
+        });
 
-    sstvAudio.play();
+        const sstvAudio = document.createElement('audio');
+        sstvAudio.src = 'assets/audio/nskdsstv.mp3';
+        sstvAudio.loop = true;
+        sstvAudio.style.display = 'none';
+        win.appendChild(sstvAudio);
+
+        sstvAudio.play();
+    } catch (e) {
+        log('Error with SSTV: ' + e.message, 'error');
+    }
 }
