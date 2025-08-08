6aad5b9002b6849f1568ccb0345f8baff1c45e06
douxxtech <douxx@douxx.tech>
2025-08-08 22:03:13 +0200

    fixed an error

--- website/assets/js/sstv.js
+++ website/assets/js/sstv.js
@@ -5,15 +5,15 @@ function showSstv() {
     try {
         const win = ClassicWindow.createWindow({
             title: 'Oh ?',
-            content: 'Try to find the secret certificate ;)\nOh yea its Robot 36 Colors btw',
+            content: 'Try to find the secret certificate ;) Oh yea its Robot 36 Colors btw',
             x: Math.round((window.innerWidth - 600) / 2),
             y: Math.round((window.innerHeight - 450) / 2),
             theme: 'dark',
             resizable: true,
             onClose: function () {
-                audio.pause();
-                audio.currentTime = 0;
-                audio.remove();
+                sstvAudio.pause();
+                sstvAudio.currentTime = 0;
+                sstvAudio.remove();
             }
         });
 
