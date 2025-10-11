baf3a820180b13177af2d4595e61bf96f6ce9135
douxxtech <douxx@douxx.tech>
2025-10-11 19:09:15 +0200

    fixed cors

--- misc/check.noskid.today/index.php
+++ misc/check.noskid.today/index.php
@@ -5,7 +5,7 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, User-Agent');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'username');
